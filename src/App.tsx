5b8aa201543bf73650efd38168870d5dc0b7e228
yoojm4718 <yoojm4718@outlook.com>
2021-09-20 11:54:37 +0900

    About State in TS-React

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-function App() {
-  return <div className="App"></div>;
+interface IState {
+  counter: number;
+}
+
+class App extends React.Component<{}, IState> {
+  state = {
+    counter: 0,
+  };
+  add() {
+    this.setState((prev) => {
+      return { counter: prev.counter + 1 };
+    });
+  }
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        {counter}
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
 }
 
 export default App;
