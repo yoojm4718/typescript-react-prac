43adc817f3ec58d2440667396a4e5b5a9455b918
yoojm4718 <yoojm4718@outlook.com>
2021-09-20 21:03:56 +0900

    About Events in TS-React

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import React from "react";
+import { Form, Input } from "./Input";
 import Number from "./Number";
 
 interface IState {
   counter: number;
+  name: string;
 }
 
 class App extends React.Component<{}, IState> {
   state = {
     counter: 0,
+    name: "",
   };
   add = () => {
     this.setState((prev) => {
       return { counter: prev.counter + 1 };
     });
   };
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
   render() {
-    const { counter } = this.state;
+    const { counter, name } = this.state;
     return (
       <div>
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange} />
+        </Form>
         <Number count={counter} />
         <button onClick={this.add}>Add</button>
       </div>
